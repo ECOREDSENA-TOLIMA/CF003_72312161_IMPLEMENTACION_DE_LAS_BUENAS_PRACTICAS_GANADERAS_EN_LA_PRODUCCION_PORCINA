ac8b89f715e6c41643b902874b089cba9ae4be5a
DaveRnC <davison.gaitan@gmail.com>
2023-08-31 15:59:44 -0500

    empaquetado

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -7,7 +7,7 @@
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="Síntesis. Implementación de Buenas Prácticas Ganaderas en la Producción Porcina inicia con la inspección de los procesos, el establecimiento de conceptos, la definición de los estándares y protocolos así como la elaboración de un plan de contingencias que a su vez incluya acciones preventivas e inmediatas soportados en un sistema de información que facilite gestión y tratamiento de datos para llevar a cabo los procesos con la trazabilidad correspondiente,  facilitando la elaboración de informes de acuerdo a la necesidad y formular el plan de mejora que permita evitar desviaciones o redireccionar la planeación hacia sus objetivos.")
       .col-auto
         a.anexo.mb-4(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
